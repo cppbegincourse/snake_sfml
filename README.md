028aab0da6ecbbbd2270f28abae70de5ceab3442
Anatoliy Sviridenko <metaluger@gmail.com>
2021-05-25 22:40:34 +0300

    Add first version that do nothing, but drawing three sprites as example how to work with SFML framework.
    Task is add code that draws field filled with grass and trees on border

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # snake_sfml
 Educational project with simple classic snake game
+
+SFML 32it framework is used. You can find it here: https://www.sfml-dev.org.
+
+Each branch is separate task for students:
+
+1. graw_field - Study loops and conditions
+
+Sprite used in this project was taken from here: https://opengameart.org/forumtopic/snake-game-art-request
---
+++ SnakeSFML/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+using namespace std;
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(960, 960), "Snake");
+	window.setFramerateLimit(60);
+
+	bool isRunning = true;
+
+	sf::Texture textureTerrain;
+	if (!textureTerrain.loadFromFile("snakeset.png")) {
+		std::cerr << "Can't load texture" << endl;
+		return 1;
+	}
+
+	sf::Sprite treeSprite;
+	treeSprite.setTexture(textureTerrain);
+	treeSprite.setTextureRect(sf::IntRect(0, 0, 64, 64));
+
+	sf::Sprite grassSprite;
+	grassSprite.setTexture(textureTerrain);
+	grassSprite.setTextureRect(sf::IntRect(0, 64, 64, 64));
+
+	sf::Sprite snakeSprite;
+	snakeSprite.setTexture(textureTerrain);
+	snakeSprite.setTextureRect(sf::IntRect(128, 128, 64, 64));
+
+	while (isRunning)
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		// Draw tree sprite
+		treeSprite.setPosition(0, 0);
+		window.draw(treeSprite);
+
+		// Draw grass sprite
+		grassSprite.setPosition(64, 0);
+		window.draw(grassSprite);
+
+		// Draw grass sprite
+		grassSprite.setPosition(128, 0);
+		window.draw(grassSprite);
+
+		// Draw snake sprite on top of grass
+		snakeSprite.setPosition(128, 0);
+		window.draw(snakeSprite);
+
+		// TODO: Draw game field filled with grass and trees on border
+
+		window.display();
+	}
+
+	return 0;
+}
